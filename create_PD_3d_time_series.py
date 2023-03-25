e9ce0a0df1c3da47008f9a99ca3991c3a6225932
lbidulka <lukebidulka@gmail.com>
2023-03-25 01:07:55 +0000

    initial adaptation of mohsens 3d ts creation

---
+++ create_PD_3d_time_series.py
@@ -0,0 +1,176 @@
+import torch
+import torch.nn
+import torch.optim
+from torch.utils import data
+from types import SimpleNamespace
+import numpy as np
+import model_confidences
+import os
+from pytorch3d.transforms import so3_exponential_map as rodrigues
+from utils.mohsen_utils.data_PD import *
+from utils.pose_utils import procrustes_torch
+
+
+config = SimpleNamespace()
+
+config.learning_rate = 0.0001
+config.BATCH_SIZE = 8
+config.N_epochs = 10
+config.NoEval = True
+
+# weights for the different losses
+config.weight_rep = 1
+config.weight_view = 1
+config.weight_teacher = 1
+config.weight_camera = 0.1
+
+backbone_2d_dataset_path = 'data/body/2d_proposals/mohsen_data_PD.npz'
+
+# --------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+
+# TODO: FINISH CHANGING THIS OVER FROM MOHSENS STRUCTURE TO MINE
+
+keypoints_PD = np.load(backbone_2d_dataset_path, allow_pickle=True)
+keypoints_PD = keypoints_PD['positions_2d'].item()
+num_joints = 15 
+
+for subject in keypoints_PD.keys():
+    for action in keypoints_PD[subject]:
+        for cam_idx in range(len(keypoints_PD[subject][action]['pos'])):
+            # Normalize camera frame
+            kps = keypoints_PD[subject][action]['pos'][cam_idx]
+            conf = keypoints_PD[subject][action]['conf'][cam_idx]
+            kps = kps[:, [0,1,2,3,4,5,6,8,9,10,11,12,13,14,15], :]
+            conf = conf[:, [0,1,2,3,4,5,6,8,9,10,11,12,13,14,15], :]
+            kps = kps - kps[:, :1, :]
+            kps = np.transpose(kps, [0, 2, 1])
+            kps = kps.reshape(-1, num_joints*2)
+            kps /= np.linalg.norm(kps, ord=2, axis=1, keepdims=True)
+            keypoints_PD[subject][action]['pos'][cam_idx] = kps
+            keypoints_PD[subject][action]['conf'][cam_idx] = conf
+
+def fetch_train(subjects, actions):
+    out_subject = []
+    out_poses_2d = []
+    out_confidences=[]
+    for i in range(2):
+        out_poses_2d.append([])
+        out_confidences.append([])
+
+    for subject in subjects:
+        for action in actions:
+                
+            poses_2d = keypoints_PD[subject][action]['pos']
+            conf_2d = keypoints_PD[subject][action]['conf']
+
+            for i in range(2): # Iterate across cameras
+                out_poses_2d[i].append(poses_2d[i])
+                out_confidences[i].append(conf_2d[i])
+
+            out_subject.append(np.ones(len(poses_2d[0])) * int(subject[1:]))
+            
+    for i in range(len(poses_2d)): # Iterate across cameras
+        out_poses_2d[i] = np.concatenate(out_poses_2d[i], axis=0)        
+        out_confidences[i] = np.concatenate(out_confidences[i], axis=0) 
+    out_subject = np.concatenate(out_subject, axis=0)
+
+    return out_poses_2d, out_confidences, out_subject
+
+subjects = [['S01'],['S02'],['S03'],['S04'],['S05'],['S06'],
+            ['S07'],['S08'],['S09'],['S10'],['S11'],
+            ['S12'],['S13'],['S14'],['S15'],['S16'],
+            ['S17'],['S18'],['S19'],['S20'],['S21'],
+            ['S22'],['S23'],['S24'],['S25'],['S26'],
+            ['S27'],['S28'],['S29'],['S30'],['S31'],
+            ['S32'],['S33'],['S34'],['S35']]
+
+# --------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+
+
+# Options
+outpath = 'data/body/3d_time_series/'
+lifter_model_path = 'model_checkpoints/body_pose/Mohsens/model_pretrain.pt'
+
+cam_names = ['54138969', '55011271', '58860488', '60457274']
+all_cams = ['cam0', 'cam1']
+cam_ch_names = ['CH3', 'CH4']
+
+camspace_reproj = True
+
+print('camspace_reproj: ', camspace_reproj)
+
+# loading the lifting network
+model_eval = model_confidences.Lifter().cuda()
+
+# Get the predictions
+with torch.no_grad():
+    checkpoint = torch.load(lifter_model_path)
+    model_eval.load_state_dict(checkpoint.state_dict())
+    model_eval.eval()
+
+    for cam_name_idx, cam in enumerate(all_cams):
+        # Output paths
+        preds_outpath = (outpath + cam_ch_names[cam_name_idx] + '/finetune_3d_camspace/') if camspace_reproj else \
+                        (outpath + cam_ch_names[cam_name_idx] + '/finetune_3d_canonspace/')
+
+        if not os.path.exists(preds_outpath):
+            print("Creating output directory: ", preds_outpath)
+            os.makedirs(preds_outpath)
+
+        print("Saving predictions to: ", preds_outpath)
+        preds_outpath += 'Predictions_'
+
+        for subj in subjects:
+            pred_save = []
+            poses_2d_train2, conf_2d_train2, out_subject2 = fetch_train(subj,['WalkingOval'])
+            my_dataset2 = H36MDataset(poses_2d_train2, conf_2d_train2, out_subject2, normalize_2d=True)
+            train_loader2 = data.DataLoader(my_dataset2, batch_size=2000, shuffle=False, num_workers=0)    
+
+            for i, sample in enumerate(train_loader2):
+                # not the most elegant way to extract the dictionary
+                poses_2d = {key:sample[key] for key in all_cams}
+
+                inp_poses = torch.zeros((poses_2d[cam].shape[0] , num_joints*2)).cuda()
+                inp_confidences = torch.zeros((poses_2d[cam].shape[0] , num_joints)).cuda()
+
+                # poses_2d is a dictionary. It needs to be reshaped to be propagated through the model.
+                cnt = 0
+                # Note we only use the first camera...
+                for b in range(poses_2d[cam].shape[0]):
+                    inp_poses[cnt] = poses_2d[cam][b]
+                    inp_confidences[cnt] = sample['confidences'][0][b]
+                    cnt += 1
+
+                pred = model_eval(inp_poses, inp_confidences)
+                pred_poses = pred[0]
+                pred_cam_angles = pred[1]
+
+                # Reproject to camera if we want
+                if camspace_reproj:
+                    # angles are in axis angle notation
+                    # use Rodrigues formula (Equations 3 and 4) to get the rotation matrix
+                    pred_rot = rodrigues(pred_cam_angles)
+
+                    # reproject to original cameras after applying rotation to the canonical poses
+                    rot_poses = pred_rot.matmul(pred_poses.reshape(-1, 3, num_joints))
+                    rot_poses = torch.transpose(rot_poses,2,1)
+
+                    rot_poses -= rot_poses[:,:1]
+                    pred_aligned = procrustes_torch(rot_poses[0:1], rot_poses)
+                    pred_save.append(pred_aligned)
+                else:
+                    pred_poses = pred_poses.cpu().reshape(-1, 3, 15)    # (B, XYZ, 15)
+                    pred_poses = np.transpose(pred_poses, [0, 2, 1])
+
+                    pred_poses -= pred_poses[:,:1]
+                    pred_aligned = procrustes_torch(pred_poses[0:1], pred_poses)
+
+                    pred_save.append(pred_poses)
+            
+            print(subj[0], end=', ')
+            # print(pred_save[0][0,:1], end=', ')
+            print(pred_save[0].shape)
+            np.save(preds_outpath + subj[0], np.concatenate(pred_save))
+    print(' done!')
--- utils/pose_utils.py
+++ utils/pose_utils.py
@@ -4,6 +4,39 @@ from pytorch3d.transforms import so3_exponential_map as rodrigues
 from utils import metrics
 
 
+def procrustes_torch(X, Y):
+    """
+    Reimplementation of MATLAB's `procrustes` function to Numpy.
+    """
+    X1=X[:,[0,1,4,7,8,9,10,13]]
+    Y1=Y[:,[0,1,4,7,8,9,10,13]]
+    batch,n, m = X1.shape
+    batch, ny, my = Y1.shape
+
+    muX = torch.mean(X1,dim=1,keepdim=True)
+    muY = torch.mean(Y1,dim=1,keepdim=True)
+
+    X0 = X1 - muX
+    Y0 = Y1 - muY
+
+    # optimum rotation matrix of Y
+    A = torch.matmul(torch.transpose(X0,-1,-2), Y0)
+    U,s,V = torch.svd(A)
+    T = torch.matmul(V, torch.transpose(U,-1,-2))
+
+    X1=X
+    Y1=Y
+    muX = torch.mean(X1,dim=1,keepdim=True)
+    muY = torch.mean(Y1,dim=1,keepdim=True)
+
+    X0 = X1 - muX
+    Y0 = Y1 - muY
+
+    Z = torch.matmul(Y0, T) + muX
+
+
+    return np.array(Z.cpu())
+
 def reshape_and_align(kpts_3d, angles, reproj=False, swap_legs=True):
     '''
     Unflattens the lifter output 3D keypoints, centres them on the hip, and procrustes aligns them to first pose
