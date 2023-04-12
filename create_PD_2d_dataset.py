3cedf156099915624e609f2bd26c370b81fabab6
lbidulka <lukebidulka@gmail.com>
2023-04-12 22:20:44 +0000

    add preds JSON filtering and pickling

--- create_PD_2d_dataset.py
+++ create_PD_2d_dataset.py
@@ -1,7 +1,9 @@
-import argparse
 import os
 import fnmatch
+import pickle
 from types import SimpleNamespace
+
+from data.body.body_dataset import get_2D_keypoints_from_alphapose_dict, MOHSEN_filter_alphapose_results
 
 # Subject ID mapping -- I HATE THIS BUT I DONT WANNA DEAL WITH MODULE IMPORTING
 subjects_ALL_id_dict = {
@@ -76,7 +78,7 @@ def get_AlphaPoses(config):
                     # if S_id == 'S29':
                     elif S_id == 'S31':
                         file_subpath += '2021-8-11/'
-                    file_subpath += 'LNR616X_ch{}_main_{}.avi'.format(ch[-1], new_sys_vid_suffixes[S_id][config.task])
+                    file_subpath += 'LNR616X_ch{}_main_{}.avi'.format(ch[-1], new_sys_vid_suffixes[S_id][config.updrs_task])
                 # Old system subjects
                 if S_id not in subjects_new_system:
                     file_subpath = subjects_All_date[subj_idx] + '/' + str(id)
@@ -89,11 +91,11 @@ def get_AlphaPoses(config):
                         break
                     file_subpath += 'CH_' + ch + '/'
                     for file in os.listdir(config.videos_path + file_subpath):
-                        if fnmatch.fnmatch(file, '*' + config.task + '*.mp4'):
+                        if fnmatch.fnmatch(file, '*' + config.updrs_task + '*.mp4'):
                             file_subpath += file
 
                 in_path = config.videos_path + file_subpath
-                out_path = config.frames_path + str(id) + '/' + config.task + '/'
+                out_path = config.dataset_path + str(id) + '/' + config.updrs_task + '/'
 
                 # Make sure alles gut
                 print("input video subpath: {} \npreds json outpath: {}".format(file_subpath, out_path))
@@ -120,29 +122,62 @@ def get_AlphaPoses(config):
                         
                         # Reformat the output json file
                         mv_cmd = "mv {}alphapose-results.json {}CH{}_alphapose-results.json".format(out_path, out_path, ch)
-                        print("Trying mv cmd: ", mv_cmd, end='\n\n')
+                        print("\nTrying mv cmd: ", mv_cmd, end='\n\n')
                         os.system(mv_cmd)
-                    print("Done with ", S_id, "CH_" + ch)
+                    print("Done with {}, CH_{}".format(S_id,ch))
+
+def compile_JSON(config):
+    '''
+    Builds a combined task dataset (JSON) of 2D pose predictions from per-subject-per-channel alphapose pred JSONs
+    '''
+    print("\nCompiling predictions into JSON...")
+    kpts_dict = {}
+    for S_id, id in subjects_ALL_id_dict.items():
+        if S_id in config.subjs_to_compile:
+            print("\n--- {}: ---".format(S_id))
+            # Construct paths
+            in_json_path = config.dataset_path + str(id) + '/' + config.updrs_task + '/'
+
+            # TODO: MAKE THIS NOT HARDCODED TO FIRST AND SECOND CHANNEL ENTRIES
+            ch1_in_json_pth =  in_json_path + 'CH' + config.chs[0] + "_alphapose-results.json"
+            ch2_in_json_pth =  in_json_path + 'CH' + config.chs[1] + "_alphapose-results.json"
+
+            if not os.path.exists(ch1_in_json_pth):
+                print("  ERR Input JSON not found: ", ch1_in_json_pth)
+            elif not os.path.exists(ch2_in_json_pth):
+                print("  ERR Input JSON not found: ", ch2_in_json_pth)
+            else:
+                kpts_dict = MOHSEN_filter_alphapose_results(in_json_path, S_id, config.updrs_task, kpts_dict)
+                print("  Successfully loaded alphapose data.")
+    # Pickle the 2d keypoints dict
+    print("\nSaving to ", config.JSON_dataset_outpath)
+    with open(config.JSON_dataset_outpath, 'wb') as handle:
+        pickle.dump(kpts_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print("Successfully created 2D keypoint dataset.")
 
 def get_config():
     config = SimpleNamespace()
     # Tasks
     config.get_2d_preds = True  # Proces videos to get 2d preds?
+    config.compile_JSON = True  # Compile 2d preds into JSON dataset file?
+
     # config.subjs_to_get_preds = subjects_All
-    # config.subjs_to_get_preds = subjects_new_system
-    config.subjs_to_get_preds = ["S29"]
+    config.subjs_to_get_preds = subjects_new_system
+    # config.subjs_to_get_preds = ["S29"]
+    config.subjs_to_compile = ['S01', 'S28', 'S29']
+    # Settings
+    config.limit_num_frames = True  # DEBUG: limit number of frames to process
+    config.lim_secs = 30 # Mohsen used 2 min per video
+    config.updrs_task = "free_form_oval"
+    # config.updrs_task = "tug_stand_walk_sit"
+    config.chs = ["003", "004"]
 
     # Paths
     config.root_dir = os.path.dirname(os.path.realpath(__file__))
     config.videos_path = "/mnt/CAMERA-data/CAMERA/CAMERA visits/Mobility Visit/Study Subjects/"
-    config.frames_path = "/mnt/CAMERA-data/CAMERA/Other/lbidulka_dataset/"
+    config.dataset_path = "/mnt/CAMERA-data/CAMERA/Other/lbidulka_dataset/"
     config.AP_dir = config.root_dir + "/AlphaPose"
-    # Settings
-    config.limit_num_frames = True  # DEBUG: limit number of frames to process
-    config.lim_secs = 5 # Mohsen used 2 min per video
-    config.task = "free_form_oval"
-    # config.task = "tug_stand_walk_sit"
-    config.chs = ["003", "004"]
+    config.JSON_dataset_outpath = config.root_dir + "/data/body/2d_proposals/" + config.updrs_task + "_2D_kpts-DEBUG.pickle"
     return config
 
 def main():
@@ -150,6 +185,8 @@ def main():
 
     if config.get_2d_preds:
         get_AlphaPoses(config)
+    if config.compile_JSON:
+        compile_JSON(config)
 
 if __name__ == '__main__':
     main()
